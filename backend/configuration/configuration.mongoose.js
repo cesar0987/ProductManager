cf0cf16768a44287f6957b6f15f4c4e045b3e0d2
cesar0987 <cesarbenitez46@gmail.com>
2024-06-05 19:26:16 -0400

    Changes

--- backend/configuration/configuration.mongoose.js
+++ backend/configuration/configuration.mongoose.js
@@ -3,9 +3,7 @@ const mongoose = require("mongoose");
 mongoose;
 async function connectDB() {
   try {
-    await mongoose.connect(
-      "mongodb+srv://cesarbenitez46:IAIryG1Lu2O18vld@productmng.aw1uwja.mongodb.net/productManager"
-    );
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Established a connection to the database");
   } catch (err) {
     console.log("Error connecting to the database", err);
