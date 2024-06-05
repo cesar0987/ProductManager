7a8be1bff724f4fae327aaa9ae3eeda4940892d3
cesar0987 <cesarbenitez46@gmail.com>
2024-06-05 19:36:58 -0400

    configuration

--- backend/configuration/configuration.mongoose.js
+++ backend/configuration/configuration.mongoose.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
-
+require("dotenv").config();
 mongoose;
 async function connectDB() {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("Established a connection to the database");
   } catch (err) {
     console.log("Error connecting to the database", err);
